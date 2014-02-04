6ed9dab614c9d6340dbd8eb765e0266ddae62af8
Rob Dobson <rob__dobson@hotmail.com>
2014-02-04 23:01:25 +0000

    Added exdiv info to config file

--- StockHoldings.py
+++ StockHoldings.py
@@ -11,16 +11,23 @@ class StockHoldings:
     stockreader = None
     stockHolding = {}
 
-    def readFromJsonFile(self, fName):
-        with open(fName, 'r') as jsonFile:
-            jsonData = json.load(jsonFile)
-            self.setupFromJsonData(jsonData)
+    # def readFromJsonFile(self, fName):
+    #     with open(fName, 'r') as jsonFile:
+    #         jsonData = json.load(jsonFile)
+    #         self.setupFromJsonData(jsonData)
             
     def setupFromConfigData(self, configData):
         if configData != None:
             if 'StockInfo' in configData:
                 self.stockHolding = configData['StockInfo']
-            
+                for stock in self.stockHolding:
+                    if not "exDivDate" in stock:
+                        stock["exDivDate"] = ""
+                    if not "exDivAmount" in stock:
+                        stock["exDivAmount"] = 0
+                    if not "paymentDate" in stock:
+                        stock["paymentDate"] = ""
+
     def getStockSymbols(self):
         return [dct['symbol'] for dct in self.stockHolding]
     
